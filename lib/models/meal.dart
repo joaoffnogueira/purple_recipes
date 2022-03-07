aefdf25e46d82ffdc3815aea5138961c20176689
joaoffnogueira <contact@joaonogueira.dev>
2022-03-07 10:55:51 -0300

    Create Meal model

---
+++ lib/models/meal.dart
@@ -0,0 +1,43 @@
+enum Complexity {
+  Simple,
+  Medium,
+  Difficult,
+}
+
+enum Cost {
+  Cheap,
+  Fair,
+  Expensive,
+}
+
+class Meal {
+  final String id;
+  final String title;
+  final String imageUrl;
+  final List<String> categories;
+  final List<String> ingredients;
+  final List<String> steps;
+  final int duration;
+  final Complexity complexity;
+  final Cost cost;
+  final bool isGlutenFree;
+  final bool isLactoseFree;
+  final bool isVegan;
+  final bool isVegetarian;
+
+  const Meal({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.categories,
+    required this.ingredients,
+    required this.steps,
+    required this.duration,
+    required this.complexity,
+    required this.cost,
+    required this.isGlutenFree,
+    required this.isLactoseFree,
+    required this.isVegan,
+    required this.isVegetarian,
+  });
+}
