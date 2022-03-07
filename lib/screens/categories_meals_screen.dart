ddd505433282f94fe6efd52cd2ceba66e5542340
joaoffnogueira <contact@joaonogueira.dev>
2022-03-07 11:00:55 -0300

    Filter meals

--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:purple_recipes/data/dummy_data.dart';
 import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
-  const CategoriesMealsScreen({Key? key})
-      : super(key: key);
+  const CategoriesMealsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final Category category = ModalRoute.of(context)?.settings.arguments as Category;
+    final Category category =
+        ModalRoute.of(context)?.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas'),
+      body: ListView.builder(
+        itemBuilder: ((context, index) {
+          return Text(categoryMeals[index].title);
+        }),
       ),
     );
   }
