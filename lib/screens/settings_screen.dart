f8e349108ae368c2e118b76ec8a11c499f41741b
joaoffnogueira <contact@joaonogueira.dev>
2022-03-08 10:31:31 -0300

    Complete settings screen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,8 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:purple_recipes/components/main_drawer.dart';
+import 'package:purple_recipes/models/settings.dart';
 
-class StettingsScreen extends StatelessWidget {
+class StettingsScreen extends StatefulWidget {
   const StettingsScreen({Key? key}) : super(key: key);
+
+  @override
+  State<StettingsScreen> createState() => _StettingsScreenState();
+}
+
+class _StettingsScreenState extends State<StettingsScreen> {
+  var settings = Settings();
+
+  Widget _createSwitch(
+    String title,
+    String subtitle,
+    bool value,
+    Function onChanged,
+  ) {
+    return SwitchListTile.adaptive(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      value: value,
+      onChanged: onChanged(),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,8 +33,46 @@ class StettingsScreen extends StatelessWidget {
         title: Text('Configurações'),
       ),
       drawer: MainDrawer(),
-      body: Center(
-        child: Text('Settings'),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Configurações',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _createSwitch(
+                  'Sem Glutén',
+                  'Só exibe receitas sem glutén',
+                  settings.isGlutenFree,
+                  (value) => setState(() => settings.isGlutenFree = value),
+                ),
+                _createSwitch(
+                  'Sem Lactose',
+                  'Só exibe receitas sem lactose',
+                  settings.isLactoseFree,
+                  (value) => setState(() => settings.isLactoseFree = value),
+                ),
+                _createSwitch(
+                  'Vegana',
+                  'Só exibe receitas veganas',
+                  settings.isVegan,
+                  (value) => setState(() => settings.isVegan = value),
+                ),
+                _createSwitch(
+                  'Vegetariana',
+                  'Só exibe receitas vegetarianas',
+                  settings.isVegetarian,
+                  (value) => setState(() => settings.isVegetarian = value),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
