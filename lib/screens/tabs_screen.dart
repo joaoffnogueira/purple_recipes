1ef5febcc163456cb9b2d2a0064bd1b70dcea231
joaoffnogueira <contact@joaonogueira.dev>
2022-03-08 09:03:27 -0300

    Change to bottom navigation bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,33 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:purple_recipes/screens/categories_screen.dart';
 import 'package:purple_recipes/screens/favorite_screen.dart';
 
-class TabScreen extends StatelessWidget {
+class TabScreen extends StatefulWidget {
   const TabScreen({Key? key}) : super(key: key);
 
   @override
+  State<TabScreen> createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  int _selectedScreenIndex = 0;
+
+  final List<Widget> _screens = [
+    CategoriesScreen(),
+    FavoriteScreen(),
+  ];
+
+  final List<String> _tabTitles = [
+    'Categorias',
+    'Favoritos',
+  ];
+
+  _selectScreen(int index) {
+    setState(() {
+      _selectedScreenIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-          appBar: AppBar(
-            title: Text('Vamos cozinhar'),
-            bottom: TabBar(tabs: [
-              Tab(
-                icon: Icon(Icons.category),
-                text: 'Categorias',
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: 'Favoritos',
-              ),
-            ]),
-          ),
-          body: TabBarView(
-            children: [
-              CategoriesScreen(),
-              FavoriteScreen(),
-            ],
-          )),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_tabTitles[_selectedScreenIndex]),
+      ),
+      body: _screens[_selectedScreenIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectScreen,
+          backgroundColor: const ColorScheme.light().primary,
+          unselectedItemColor: Colors.white,
+          selectedItemColor: ColorScheme.light().secondary,
+          currentIndex: _selectedScreenIndex,
+          type: BottomNavigationBarType.shifting,
+          items: [
+            BottomNavigationBarItem(
+              backgroundColor: const ColorScheme.light().primary,
+              icon: Icon(Icons.category),
+              label: 'Categorias',
+            ),
+            BottomNavigationBarItem(
+              backgroundColor: const ColorScheme.light().primary,
+              icon: Icon(Icons.star),
+              label: 'Categorias',
+            ),
+          ]),
     );
   }
 }
